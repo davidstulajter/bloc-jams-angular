10e17c4c6adbad67457679d7bfc130dcbf7e2ce1
David Stulajter <davidstulajter@gmail.com>
2016-06-20 20:27:59 -0500

    services part one cp6 completed, updating album controller with factory service

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -1,4 +1,7 @@
-// Album example first
+(function() {
+    function Fixtures() {
+        var Fixtures = {};
+
 var albumPicasso = {
     title: 'The Colors',
     artist: 'Pablo Picasso',
@@ -6,15 +9,14 @@ var albumPicasso = {
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21' },
-        { title: 'Magenta', duration: '2:15' }
+        { title: 'Blue', duration: '4:26', audioUrl: '/assets/music/blue' },
+        { title: 'Green', duration: '3:14', audioUrl: '/assets/music/green' },
+        { title: 'Red', duration: '5:01', audioUrl: '/assets/music/red' },
+        { title: 'Pink', duration: '3:21', audioUrl: '/assets/music/pink' },
+        { title: 'Magenta', duration: '2:15', audioUrl: '/assets/music/magenta' }
     ]
 };
 
-// Album example second
 var albumMarconi = {
     title: 'The Telephone',
     artist: 'Guglielmo Marconi',
@@ -30,3 +32,14 @@ var albumMarconi = {
     ]
 };
 
+Fixtures.getAlbum = function() {
+    return albumPicasso;
+};
+
+        return Fixtures;
+    }
+
+    angular
+        .module('blocJams')
+        .factory('Fixtures', Fixtures);
+})();
